78551a7301277e47327b4acee506b1b02293055a
achoi616 <achoi616@gmail.com>
2020-06-17 03:05:40 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -9,14 +9,14 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
-    puts "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
-    puts "HAM HAM HAM IN MY TUMMY"
+  if num == 1
+    "YUM YUM MUNCH MUNCH MUNCH"
+  elsif num == 2
+    "HAM HAM HAM IN MY TUMMY"
   end
 end
 
 def runner
-  puts prompt_user
-  puts selection(get_user_input)
+  prompt_user
+  selection(get_user_input)
 end
